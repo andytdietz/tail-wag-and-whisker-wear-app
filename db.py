7a4425616643f176384b818348ae2dc5ccbc4c55
andytdietz <andytdietz@gmail.com>
2024-04-12 11:03:14 -0600

    change clohting to outfits

--- db.py
+++ db.py
@@ -11,12 +11,12 @@ def initial_setup():
     conn = connect_to_db()
     conn.execute(
         """
-        DROP TABLE IF EXISTS clothing;
+        DROP TABLE IF EXISTS outfits;
         """
     )
     conn.execute(
         """
-        CREATE TABLE clothing (
+        CREATE TABLE outfits (
           id INTEGER PRIMARY KEY NOT NULL,
           name TEXT,
           animal_id INTEGER,
@@ -28,17 +28,17 @@ def initial_setup():
     conn.commit()
     print("Table created successfully")
 
-    clothing_seed_data = [
+    outfits_seed_data = [
         ("Cowboy", "1", 13.98, "https://m.media-amazon.com/images/I/51VmmRDO1EL.__AC_SX300_SY300_QL70_FMwebp_.jpg"),
         ("Rambo Dog", "2", 27.87, "https://m.media-amazon.com/images/I/7167FaSjkWL._AC_SY679_.jpg"),
         ("Cowboy", "2", 12.99, "https://m.media-amazon.com/images/I/41EakiB0FWL.__AC_SX300_SY300_QL70_FMwebp_.jpg")
     ]
     conn.executemany(
         """
-        INSERT INTO clothing (name, animal_id, price)
+        INSERT INTO outfits (name, animal_id, price)
         VALUES (?,?,?)
         """,
-        clothing_seed_data,
+        outfits_seed_data,
     )
     conn.commit()
     print("Seed data created successfully")
